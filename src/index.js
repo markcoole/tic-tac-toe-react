8a782921058d90099545c0c010ebe5be0aaf6905
Mark Coole <mark.coole@btinternet.com>
2018-06-18 22:14:24 +0100

    Taking Turns

--- src/index.js
+++ src/index.js
@@ -15,13 +15,18 @@ function Square(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
       squares[i] = 'X';
-      this.setState({squares: squares});
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -31,7 +36,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
